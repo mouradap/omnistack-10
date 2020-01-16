b87b2ba5d8a2d66aa97be615f7c1ba5638ff6de4
Denis Moura <drmouradap@gmail.com>
2020-01-16 20:10:54 -0300

    mobile v 0.001

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+
+
+function Main() {
+    const [currentRegion, setCurrentRegion] = useState(null);
+
+    useEffect(() => {
+        async function loadInitialPosition() {
+            const { granted } = await requestPermissionsAsync();
+
+            if (granted) {
+                const {coords} = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+                const { latitude, longitude } = coords;
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                })
+
+
+                
+            }
+        }
+        loadInitialPosition();
+    }, []);
+
+    if (!currentRegion) {
+        return null;
+    }
+
+    return (
+        <MapView initialRegion={currentRegion} style={styles.map} >
+            < Marker coordinate={{latitude: -7.9976883, longitude: -34.8404644}}>
+                <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/39701076?s=460&v=4'}}/>
+
+            </Marker>
+        </MapView>
+    ) 
+}
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#FFF'
+    }
+})
+
+export default Main;
