9ed1fdd1cd8361a98c6479e4fe612ddd035e246d
Denis Moura <drmouradap@gmail.com>
2020-01-16 21:08:34 -0300

    mobile version 0.002

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { MaterialIcons } from '@expo/vector-icons';
 
 
-function Main() {
+
+function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
 
     useEffect(() => {
@@ -36,12 +38,44 @@ function Main() {
     }
 
     return (
+        <>
         <MapView initialRegion={currentRegion} style={styles.map} >
             < Marker coordinate={{latitude: -7.9976883, longitude: -34.8404644}}>
                 <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/39701076?s=460&v=4'}}/>
+                <Callout onPress={() => {
+                    //navegação
+                    navigation.navigate('Profile', {
+                        github_username: 'mouradap'
+                    });
+                }}>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}> Denis Moura </Text>
+                        <Text style={styles.devBio}> Master of Science; PhD Candidate; Bioinformatician; Software Developer. </Text>
+                        <Text style={styles.devTechs}>Python, NodeJS, React, React Native </Text>
+                    </View>
 
+                </Callout>
             </Marker>
         </MapView>
+
+        <View style={styles.searchForm}>
+            <TextInput
+             style={styles.searchInput}
+             placeHolder="Buscar Devs por Techs"
+             placeHolderTextColor="#999"
+             autoCapitalize="words"
+             autoCorrect={false}
+             />
+
+            <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                <MaterialIcons name="my-location" size = {20} color='#FFF'/>
+
+            </TouchableOpacity>
+
+
+        </View>
+
+        </>
     ) 
 }
 
@@ -55,6 +89,58 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         borderWidth: 4,
         borderColor: '#FFF'
+    },
+    callout: {
+        width: 260,
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+    devTechs: {
+        marginTop: 5,
+    },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4Dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15
     }
 })
 
